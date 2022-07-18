b1232119167841dd929fcade37a8353029118f67
Michael Fischer <79065779+mfischercodes@users.noreply.github.com>
2022-07-18 16:13:22 -0700

    signup page complete

--- app/src/main/java/com/sfu/useify/MainActivity.kt
+++ app/src/main/java/com/sfu/useify/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.sfu.useify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onSignUpClicked(view : View){
+        val newScreen : Intent = Intent(this, SignupActivity::class.java)
+        startActivity(newScreen)
     }
 }
