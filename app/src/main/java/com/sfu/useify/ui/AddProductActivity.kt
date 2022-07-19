72b56396e0b972ff9fadd5c21e55de970c11ed45
dza59 <danni_zhang_2@sfu.ca>
2022-07-19 13:17:50 -0500

    move addProduct file into a ui folder

--- app/src/main/java/com/sfu/useify/ui/AddProductActivity.kt
+++ app/src/main/java/com/sfu/useify/ui/AddProductActivity.kt
@@ -1,11 +1,15 @@
-package com.sfu.useify
+package com.sfu.useify.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sfu.useify.R
 
 class AddProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
+
+
+
     }
 }
