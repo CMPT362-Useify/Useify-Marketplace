5da77dbad35cf734c625bfcd079443a5d10102c5
Gaurav Modi <gauravmodi2003@gmail.com>
2022-07-19 01:29:24 -0700

    UI for search/categories and product details

--- app/src/main/java/com/sfu/useify/MainActivity.kt
+++ app/src/main/java/com/sfu/useify/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.sfu.useify
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun onSearchClicked(view: View) {
+        val intent = Intent(this, SearchCategoriesActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onDetailsClicked(view: View) {
+        val intent = Intent(this, ProductDetailActivity::class.java)
+        startActivity(intent)
     }
 }
