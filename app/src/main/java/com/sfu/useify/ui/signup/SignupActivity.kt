99d1ef2c78200e5b3d0f2e791e7dcf61554a9030
Michael Fischer <79065779+mfischercodes@users.noreply.github.com>
2022-07-28 21:39:20 -0700

    sign up page database connected

--- app/src/main/java/com/sfu/useify/ui/signup/SignupActivity.kt
+++ app/src/main/java/com/sfu/useify/ui/signup/SignupActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
@@ -14,6 +15,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModelProvider
 import com.sfu.useify.*
+import com.sfu.useify.database.usersViewModel
+import com.sfu.useify.models.User
 import com.sfu.useify.ui.browse.BrowseActivity
 import java.io.File
 
@@ -25,10 +28,25 @@ class SignupActivity : AppCompatActivity() {
     private lateinit var imgUri: Uri
     private val imgFileName = "signUpImage.jpg"
 
+    private lateinit var firstName : EditText
+    private lateinit var lastName : EditText
+    private lateinit var emailAddress : EditText
+    private lateinit var phoneNumber : EditText
+
+    private lateinit var userViewModel: usersViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         Util.checkPermissions(this)
+
+        userViewModel = usersViewModel()
+
+
+        firstName = findViewById(R.id.signup_first_name)
+        lastName = findViewById(R.id.signup_last_name)
+        emailAddress = findViewById(R.id.signup_first_name)
+        phoneNumber = findViewById(R.id.signup_first_name)
 
         profileImage = findViewById(R.id.signup_image)
 
@@ -80,6 +98,17 @@ class SignupActivity : AppCompatActivity() {
     }
 
     fun onSignupSaveClicked(view : View) {
+        val name : String = firstName.text.toString() + (lastName.text.toString())
+        val first = firstName.text.toString()
+        val last = lastName.text.toString()
+        val email = emailAddress.text.toString()
+        val phone = phoneNumber.text.toString()
+
+
+        val newUser = User(name, first, last, email, phone)
+        userViewModel.addUser(newUser, "1")
+
+
         val newScreen : Intent = Intent(this, BrowseActivity::class.java)
         startActivity(newScreen)
     }
