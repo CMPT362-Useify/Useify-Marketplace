6e57e6271acdc94a07542e77e8056b23c94e91cf
Gaurav Modi <Gauravmodi2003@gmail.com>
2022-07-29 20:37:08 -0700

    added chat views, working on search

---
+++ app/src/main/java/com/sfu/useify/ui/chat/ChatActivity.kt
@@ -0,0 +1,13 @@
+package com.sfu.useify.ui.chat
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChatActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView()
+
+
+    }
+}
---
+++ app/src/main/java/com/sfu/useify/ui/chat/ChatMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.sfu.useify.ui.chat
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChatMenuActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView()
+    }
+}
