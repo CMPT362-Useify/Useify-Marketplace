5d55f73bae10b93e239ad68db01bd63501e299d7
James Phan <nhat_huy_phan@sfu.ca>
2022-08-05 12:24:37 -0700

    Update signup

--- app/src/main/java/com/sfu/useify/ui/authentication/signup/SignupActivity.kt
+++ app/src/main/java/com/sfu/useify/ui/authentication/signup/SignupActivity.kt
@@ -9,6 +9,7 @@ import android.provider.MediaStore
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -16,6 +17,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 import com.sfu.useify.*
 import com.sfu.useify.database.usersViewModel
 import com.sfu.useify.models.User
@@ -30,9 +33,13 @@ class SignupActivity : AppCompatActivity() {
     private lateinit var imgUri: Uri
     private val imgFileName = "signUpImage.jpg"
 
+    private lateinit var auth: FirebaseAuth
+
+
     private lateinit var firstName : EditText
     private lateinit var lastName : EditText
     private lateinit var emailAddress : EditText
+    private lateinit var password : EditText
     private lateinit var phoneNumber : EditText
 
     private lateinit var userViewModel: usersViewModel
@@ -44,11 +51,14 @@ class SignupActivity : AppCompatActivity() {
 
         userViewModel = usersViewModel()
 
+        auth = FirebaseAuth.getInstance()
+
 
         firstName = findViewById(R.id.signup_first_name)
         lastName = findViewById(R.id.signup_last_name)
-        emailAddress = findViewById(R.id.signup_first_name)
-        phoneNumber = findViewById(R.id.signup_first_name)
+        emailAddress = findViewById(R.id.signup_email)
+        password = findViewById(R.id.signup_password)
+        phoneNumber = findViewById(R.id.signup_phone_number)
 
         profileImage = findViewById(R.id.signup_image)
 
@@ -115,15 +125,25 @@ class SignupActivity : AppCompatActivity() {
         val first = firstName.text.toString()
         val last = lastName.text.toString()
         val email = emailAddress.text.toString()
+        val passw = password.text.toString()
         val phone = phoneNumber.text.toString()
 
 
         val newUser = User(name, first, last, email, phone)
-        userViewModel.addUser(newUser, "1")
+        //userViewModel.addUser(newUser, "1")
+        println("debug: pw = " + passw + " email= " +email)
+        auth.createUserWithEmailAndPassword(email, passw).addOnCompleteListener(this, OnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+                val user = auth.currentUser
+                println("debug: " + user..toString())
+            }
+        })
 
 
-        val newScreen : Intent = Intent(this, BrowseActivity::class.java)
-        startActivity(newScreen)
     }
 
     fun onSignupCancelClicked(view : View) {
