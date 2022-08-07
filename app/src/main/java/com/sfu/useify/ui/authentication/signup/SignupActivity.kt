1b8fa63a61aa7d855aec95ce9520c94cb3e176d9
James Phan <nhat_huy_phan@sfu.ca>
2022-08-07 13:01:43 -0700

    Fix invalid email exceptions

--- app/src/main/java/com/sfu/useify/ui/authentication/signup/SignupActivity.kt
+++ app/src/main/java/com/sfu/useify/ui/authentication/signup/SignupActivity.kt
@@ -1,5 +1,6 @@
 package com.sfu.useify.ui.authentication.signup
 
+import com.sfu.useify.R
 import android.app.Activity
 import android.content.Intent
 import android.content.res.Configuration
@@ -20,13 +21,16 @@ import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.sfu.useify.*
 import com.sfu.useify.database.usersViewModel
 import com.sfu.useify.models.User
 import com.sfu.useify.ui.authentication.login.LoginActivity
-import com.sfu.useify.ui.browse.BrowseActivity
 import java.io.File
+
 
 class SignupActivity : AppCompatActivity() {
 
@@ -38,7 +42,7 @@ class SignupActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
-
+    private lateinit var userName : EditText
     private lateinit var firstName : EditText
     private lateinit var lastName : EditText
     private lateinit var emailAddress : EditText
@@ -56,7 +60,7 @@ class SignupActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
-
+        userName = findViewById(R.id.signup_user_name)
         firstName = findViewById(R.id.signup_first_name)
         lastName = findViewById(R.id.signup_last_name)
         emailAddress = findViewById(R.id.signup_email)
@@ -124,7 +128,7 @@ class SignupActivity : AppCompatActivity() {
     }
 
     fun onSignupSaveClicked(view : View) {
-        val name : String = firstName.text.toString() + (lastName.text.toString())
+        val name = userName.text.toString()
         val first = firstName.text.toString()
         val last = lastName.text.toString()
         val email = emailAddress.text.toString()
@@ -135,28 +139,47 @@ class SignupActivity : AppCompatActivity() {
         val newUser = User(name, first, last, email, phone)
         //userViewModel.addUser(newUser, "1")
         println("debug: pw = " + passw + " email= " +email)
-        auth.createUserWithEmailAndPassword(email, passw).addOnCompleteListener(this, OnCompleteListener { task ->
-            if (task.isSuccessful ) {
-                val user = auth.currentUser
-                val userID = user?.uid.toString()
-                val profileUpdates = userProfileChangeRequest {
-                    displayName = newUser.username
-                }
-                user?.updateProfile(profileUpdates)?.addOnCompleteListener { task ->
+        if (passw.isEmpty() || email.isEmpty()) {
+            Toast.makeText(this, "Please enter password and email", Toast.LENGTH_LONG).show()
+        } else {
+            auth.createUserWithEmailAndPassword(email, passw)
+                .addOnCompleteListener(this, OnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        Log.d("firebase", "User profile updated.")
+                        val user = auth.currentUser
+                        val userID = user?.uid.toString()
+                        val profileUpdates = userProfileChangeRequest {
+                            displayName = newUser.username
+                        }
+                        user?.updateProfile(profileUpdates)?.addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Log.d("firebase", "User profile updated.")
+                            }
+                        }
+                        userViewModel.addUser(newUser, userID)
+                        println("debug: " + userID.toString())
+                        Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        try {
+                            throw task.exception!!
+                        } catch (e: FirebaseAuthWeakPasswordException) {
+                            Toast.makeText(baseContext, "Authentication failed: Your password is too weak, it must have at least 6 characters" ,
+                                Toast.LENGTH_LONG).show()
+                        } catch (e: FirebaseAuthInvalidCredentialsException) {
+                            Toast.makeText(baseContext, "Authentication failed: Your email or password is invalid"  ,
+                                Toast.LENGTH_LONG).show()
+                        } catch (e: FirebaseAuthUserCollisionException) {
+                            Toast.makeText(baseContext, "Authentication failed: This email is already used by another user, please try another email" ,
+                                Toast.LENGTH_LONG).show()
+                        } catch (e: Exception) {
+                            Log.e("Authentication", e.message!!)
+                        }
+
                     }
-                }
-                userViewModel.addUser(newUser, userID)
-                println("debug: " + userID.toString())
-                Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show()
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-
-
-            }
-        })
+                })
+        }
 
 
     }
