721ee9c245daf496c4ad076d81c12ffac1f60eb4
dza59 <danni_zhang_2@sfu.ca>
2022-08-08 00:46:49 -0700

    update Grid view style

--- app/src/main/java/com/sfu/useify/MainActivity.kt
+++ app/src/main/java/com/sfu/useify/MainActivity.kt
@@ -7,10 +7,15 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.AdapterView
+import android.widget.GridView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.MutableLiveData
 import com.sfu.useify.database.conversationsViewModel
+import com.sfu.useify.database.productsViewModel
+import com.sfu.useify.models.Product
 import com.sfu.useify.resetPages.GeneralSettings
 import com.sfu.useify.resetPages.ResetPassword
 import com.sfu.useify.ui.addProduct.AddEditProductActivity
@@ -20,13 +25,30 @@ import com.sfu.useify.ui.categories.SearchCategoriesActivity
 import com.sfu.useify.ui.chat.ChatMenuActivity
 import com.sfu.useify.ui.productDetails.ProductDetailActivity
 import com.sfu.useify.ui.authentication.signup.SignupActivity
+import com.sfu.useify.ui.browse.ProfileAdapter
 import com.sfu.useify.ui.savedProducts.SavedProductsActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var gridView: GridView
+    private lateinit var titles: MutableList<String>
+    private lateinit var Ids: MutableList<String>
+    private lateinit var price: MutableList<Float>
+    private lateinit var images: MutableList<String>
+    private lateinit var productDatabase: productsViewModel
+    private lateinit var myArrayList: MutableLiveData<List<Product>>
+    private lateinit var myDate: MutableList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_browse)
+
+        getGridData()
+        setGridView()
+        gridViewOnClickListener()
     }
 
     fun changeTheme(view: View){
@@ -38,6 +60,67 @@ class MainActivity : AppCompatActivity() {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
 
+    }
+
+
+
+    /**
+     * Description: gets names and images from database
+     * Post-condition: changes names and images variables
+     */
+    private fun getGridData(){
+        productDatabase = productsViewModel()
+        myArrayList = productDatabase.getAllProducts()
+        titles = ArrayList()
+        Ids = ArrayList()
+        price = ArrayList()
+        images = ArrayList()
+        myDate = ArrayList()
+
+        myArrayList.observe(this) {
+            val myProduct : List<Product>? = it
+            if (myProduct != null) {
+                for(product in myProduct) {
+                    titles.add(product.name)
+                    Ids.add(product.productID)
+                    price.add(product.price.toFloat())
+                    images.add(product.image)
+                    val formatter = SimpleDateFormat("MMMM-dd-yyyy", Locale.CANADA)
+                    val dateString: String = formatter.format(Date(product.createAt))
+                    myDate.add(dateString)
+                }
+                setGridView()
+            }
+
+        }
+    }
+
+    /**
+     * Description: sets gridview elements with name and photo of each profile
+     * Post-condition: gridview is set to xml element and adapter is set
+     */
+    private fun setGridView() {
+        gridView = findViewById(R.id.browseGridView)
+        val profileAdapter = ProfileAdapter(this, titles, price, images, myDate)
+        gridView.adapter = profileAdapter
+    }
+
+    /**
+     * Description: opens main menu on grid item selected
+     * Post-condition: current user changed to selected item
+     */
+    private fun gridViewOnClickListener() {
+        gridView.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+
+                val intent = Intent(applicationContext, ProductDetailActivity::class.java)
+                var key = view.context.getString(R.string.key_product_clicked)
+                intent.putExtra(
+                    key,
+                    Ids[position]
+                )
+                startActivity(intent)
+            }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
